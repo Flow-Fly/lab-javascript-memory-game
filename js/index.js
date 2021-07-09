60e5778de9cd9a56f7142e1b98a26aa741d8c0e9
Flow-Fly <florian.aube@gmail.com>
2021-07-09 16:02:55 +0200

    resolved bug and finiched

--- js/index.js
+++ js/index.js
@@ -28,8 +28,72 @@ const cards = [
 const memoryGame = new MemoryGame(cards);
 const pairsClicked = document.getElementById('pairs-clicked')
 const pairsGuessed = document.getElementById('pairs-guessed')
-window.addEventListener('load', (event) => {
+const restartBtn = document.getElementById('restart')
+const endCard = document.querySelector('.restart.hidden')
+
+window.addEventListener('load', () => {
+  
+  start()
+
+
+})
+
+function checkTwoCards() {
+  return memoryGame.pickedCards.length === 2;
+}
+
+function lockCards() {
+  let locked = document.querySelectorAll('.turned')
+  locked.forEach(card => {
+    card.classList.add('blocked')
+  })
+
+  updateScore()
+}
+
+function returnCards() {
+  setTimeout(() => {
+    let turn = document.querySelectorAll('.turned:not(.blocked)')
+    turn.forEach(card => {
+      card.classList.toggle('turned')
+  })
+  pairsClicked.textContent = memoryGame.pairsClicked
+  memoryGame.pickedCards = []
+  },1000)
+}
+
+function endGame () {
+  endCard.classList.toggle('hidden')
+  restartBtn.onclick = () => {
+    resetGame()
+    
+  }
+}
+
+
+function resetGame() {
+  const cards = document.querySelectorAll('.card')
+  cards.forEach(card => {
+    card.classList.remove('blocked')
+    card.classList.toggle('turned')
+    memoryGame.pickedCards = []
+    memoryGame.pairsClicked = 0
+    memoryGame.pairsGuessed = 0
+    updateScore()
+  })
+  endCard.classList.toggle('hidden')
+  start()
+}
+
+function updateScore() {
+  pairsClicked.textContent = memoryGame.pairsClicked
+  pairsGuessed.textContent = memoryGame.pairsGuessed
+  memoryGame.pickedCards = []
+}
+
+function start() { 
   memoryGame.shuffleCards()
+
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -42,8 +106,8 @@ window.addEventListener('load', (event) => {
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
+  
 
-  // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
 
@@ -62,36 +126,9 @@ window.addEventListener('load', (event) => {
       }
       if (memoryGame.checkIfFinished()) {
         setTimeout(() => {
-          alert('You wooooooon')
-          memoryGame.shuffleCards()
-        },500)
+          endGame()
+        },1000)
       }
     })
   })
-})
-
-function checkTwoCards() {
-  return memoryGame.pickedCards.length === 2;
-}
-
-function lockCards() {
-  let locked = document.querySelectorAll('.turned')
-  locked.forEach(card => {
-    card.classList.add('blocked')
-  })
-
-  pairsClicked.textContent = memoryGame.pairsClicked
-  pairsGuessed.textContent = memoryGame.pairsGuessed
-  memoryGame.pickedCards = []
-}
-
-function returnCards() {
-  setTimeout(() => {
-    let turn = document.querySelectorAll('.turned:not(.blocked)')
-    turn.forEach(card => {
-      card.classList.toggle('turned')
-  })
-  pairsClicked.textContent = memoryGame.pairsClicked
-  memoryGame.pickedCards = []
-  },1000)
 }
